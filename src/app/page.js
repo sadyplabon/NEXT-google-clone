b6c8d116b7b2f65a55c032ef87e4849b5f4fd6b5
sadyplabon <plabon.pen@gmail.com>
2024-02-03 21:55:23 +0600

    add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      {/* header */}
+
       <HomeHeader />
+
+      {/* body */}
+
+      <div className="flex flex-col items-center mt-24">
+        <Image src={"/Google.png"} width={"300"} height={"100"} />
+        <HomeSearch />
+      </div>
     </>
   );
 }
